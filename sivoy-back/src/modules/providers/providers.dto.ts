5fc2d0e822fcc7b531f6ee276436442b6899f332
victor12g <victorgandarillas12@gmail.com>
2024-10-02 12:24:38 -0300

    logica providerslista

---
+++ sivoy-back/src/modules/providers/providers.dto.ts
@@ -0,0 +1,23 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateProviderDto {
+
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+  
+    @IsNotEmpty()
+    @IsString()
+    description: string;
+}
+
+export class UpdateProviderDto {
+
+    @IsNotEmpty()
+    @IsString()
+    name?: string;
+  
+    @IsNotEmpty()
+    @IsString()
+    description?: string;
+}
