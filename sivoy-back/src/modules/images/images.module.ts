9e8f673d38db684df394785f34cc929f8fbb7c3d
OGMaximo <carmaxi92@gmail.com>
2024-10-03 18:01:46 -0300

    CRUD de imagenes y cambios en la entidad

--- sivoy-back/src/modules/images/images.module.ts
+++ sivoy-back/src/modules/images/images.module.ts
@@ -4,9 +4,10 @@ import { ImagesService } from './images.service';
 import { ImagesRepository } from './images.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Image } from 'src/entities/images.entity';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Image])],
+  imports: [TypeOrmModule.forFeature([Image]), UsersModule],
   controllers: [ImagesController],
   providers: [ImagesService, ImagesRepository],
 })
