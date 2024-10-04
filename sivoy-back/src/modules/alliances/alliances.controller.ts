756a866ed637ff98eefb92876b63b19d3df8e1a5
victor12g <victorgandarillas12@gmail.com>
2024-10-04 09:45:54 -0300

    logica alianzas lista y arreglo a team

--- sivoy-back/src/modules/alliances/alliances.controller.ts
+++ sivoy-back/src/modules/alliances/alliances.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { AlliancesService } from './alliances.service';
+import { CreateAllianceDto, UpdateAllianceDto } from './alliances.dto';
 
 @Controller('alliances')
-export class AlliancesController {}
+export class AlliancesController {
+    constructor(private readonly alliancesService: AlliancesService) {}
+
+    @Get()
+    getAllAlliances() {
+        return this.alliancesService.getAllAlliances();
+    }
+
+    @Get(':id')
+    getAllianceById(@Param('id') id: string) {
+        return this.alliancesService.getAllianceById(id);
+    }
+
+    @Post()
+    createAlliance(@Body() alliance: CreateAllianceDto) {
+        return this.alliancesService.createAlliance(alliance);
+    }
+
+    @Put(':id')
+    updateAlliance(@Param('id') id: string, @Body() alliance: UpdateAllianceDto) {
+        return this.alliancesService.updateAlliance(id, alliance);
+    }
+
+    @Delete(':id')
+    deleteAlliance(@Param('id') id: string) {
+        return this.alliancesService.deleteAlliance(id);
+    }
+
+    @Patch(':id')
+    restoreAlliance(@Param('id') id: string) {
+        return this.alliancesService.restoreAlliance(id);
+    }
+}
